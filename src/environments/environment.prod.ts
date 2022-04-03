01a4af9c58bb939106ffa6ec7815d2546003fd1f
Jonathan <jonathangani279@gmail.com>
2022-04-03 18:00:05 +0700

    change production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://moodscape-admin-app.web.app',
-  apiUrl: 'http://139.255.65.93:5000/api/graphql'
+  apiUrl: 'https://moodscape-api.herokuapp.com/api/graphql'
 };
